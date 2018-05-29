44813ddf48c53e6919814b9ceec9c0d57318233b
juangallostra <juangallostra@gmail.com>
2018-05-29 18:37:59 +0200

    Plot first height estimated from baro to not shadow other plots

--- plot_log.py
+++ plot_log.py
@@ -17,10 +17,10 @@ ax1.set_title("acceleration, velocity and height estimation")
 ax1.set_xlabel('Sample')
 ax1.set_ylabel('Value')
 
+ax1.plot(height_from_baro, c='y', label='Height from baro')
 ax1.plot(accel, c='b', label='Acceleration')
 ax1.plot(vel, c='r', label='Velocity')
 ax1.plot(height, c='g', label='Height')
-ax1.plot(height_from_baro, c='y', label='Height from baro')
 
 leg = ax1.legend()
 
