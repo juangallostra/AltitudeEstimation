069e379bf856d359d3f0da035c5d2e9341a8b48b
juangallostra <juangallostra@gmail.com>
2018-05-30 23:23:11 +0200

    Improve readability by renaming and using numpy methods

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ GX_BIAS = 0.02
 GY_BIAS = 0.01
 GZ_BIAS = 0.01
 
-AX_BIAS = 0.1962
+AX_BIAS = 0.0981
 AY_BIAS = 0.1962
 AZ_BIAS = 0
 
 # standard deviation of sensors - educated guess
 sigma_accel = 0.2
 sigma_gyro = 0.2
-sigma_baro = 1.25
+sigma_baro = 10
 
 # gravity in m/s^2 
 g = 9.81
 
 # more guesses - it has to be less that one for sure (I think)
-ca = 0.2
+ca = 0.5
 
 # gain of complementary filter
 Kc = np.array([np.sqrt(2*(sigma_accel/sigma_baro)), sigma_accel/sigma_baro]) 
@@ -59,7 +59,7 @@ def millibars_to_meters(mb, ground_height=0):
 	"""
 	Relate pressure to height
 	"""
-	return 44330*(1 - (mb/1013.25)**0.19) - ground_height
+	return 44330 * (1 - (mb/1013.25)**0.19) - ground_height
 
 
 def calibrate(baro, ground_pressure, HISTORY, count):
@@ -91,51 +91,50 @@ def get_sensor_data(serial_obj):
 	return accel, gyro, baro
 
 
-def get_prediction_covariance(v, t, sigma):
+def get_prediction_covariance(state_prev, t, sigma_gyro):
 	"""
 	Get the prediction covariance matrix
 	"""
-	Sigma = (sigma**2)*np.identity(3) 
-	return (-t**2)*skew(v).dot(Sigma).dot(skew(v))
+	Sigma = np.power(sigma_gyro, 2)*np.identity(3) 
+	return -np.power(t, 2)*skew(state_prev).dot(Sigma).dot(skew(state_prev))
 
 
-def get_measurement_covariance(ca, a, sigma):
+def get_measurement_covariance(ca, a_sensor_prev, sigma_accel):
 	"""
 	Get the measurement covariance matrix
 	"""
-	Sigma = (sigma**2)*np.identity(3)
-	return Sigma + (1.0/3)*(ca**2)*la.norm(a)*np.identity(3)
+	Sigma = np.power(sigma_accel, 2)*np.identity(3)
+	return Sigma + (1.0/3)*np.power(ca, 2)*la.norm(a_sensor_prev)*np.identity(3)
 
 
-def predict_state(gyro, z, T):
+def predict_state(gyro_prev, z, T):
 	"""
 	Predict the state evolution of the system one step ahead
 	"""
-	return (np.identity(3) - T*skew(gyro)).dot(z)
+	return (np.identity(3) - T*skew(gyro_prev)).dot(z)
 
 
-def predict_error_covariance(gyro, z, T, P, sigma_gyro):
+def predict_error_covariance(gyro_prev, z_prev, T, P, sigma_gyro):
 	"""
 	Project the covariance matrix from the data we have 
 	"""
-	I = np.identity(3) 
-	Q = get_prediction_covariance(z, T, sigma_gyro) # Prediction covariance matrix
-	return (I - T*skew(gyro)).dot(P).dot((I - T*skew(gyro)).T) + Q 
+	Q = get_prediction_covariance(z_prev, T, sigma_gyro) # Prediction covariance matrix
+	return (np.identity(3) - T*skew(gyro_prev)).dot(P).dot((np.identity(3) - T*skew(gyro_prev)).T) + Q 
 
 
-def update_kalman_gain(P, H, ca, a, sigma_accel):
+def update_kalman_gain(P, H, ca, a_sensor_prev, sigma_accel):
 	"""
 	Compute the gain from the predicted error covariance matrix
 	and the measurement covariance matrix
 	"""
-	R = get_measurement_covariance(ca, a, sigma_accel)
+	R = get_measurement_covariance(ca, a_sensor_prev, sigma_accel)
 	return P.dot(H.T).dot(la.inv(H.dot(P).dot(H.T) + R))
 
-def update_state_with_measurement(z, K, measurement, H):
+def update_state_with_measurement(predicted_state, K, measurement, H):
 	"""
 	Update the state estimate with the measurement
 	"""
-	return z + K.dot(measurement - H.dot(z))
+	return predicted_state + K.dot(measurement - H.dot(predicted_state))
 
 def update_error_covariance(P, H, K):
 	"""
@@ -151,7 +150,7 @@ def ZUPT(a_earth, vertical_vel, zupt_history, zupt_counter):
 	The zero speed is detected when during the last 12 readings
 	the estimated acceleration is lower than the threshold  
 	"""
-	THRESHOLD = 0.2
+	THRESHOLD = 0.3
 	if len(zupt_history) > zupt_counter % 12:
 		del zupt_history[zupt_counter % 12]
 	zupt_history.insert(zupt_counter % 12, a_earth)
@@ -168,7 +167,7 @@ serial_com = serial.Serial(PORT, BAUDRATE)
 prev_time = time.time()
 ZUPT_counter = 0
 z = np.array([0, 0, 1]) # assume earth and body frame have same orientation
-a = np.array([0, 0, 0]) # the components of the acceleration are all 0
+a_sensor = np.array([0, 0, 0]) # the components of the acceleration are all 0
 gyro_prev = np.array([0, 0, 0])
 P = np.array([[100, 0, 0],[0, 100, 0],[0, 0, 100]]) # initial error covariance matrix
 H = g*np.identity(3) # observation transition matrix
@@ -203,25 +202,26 @@ while True:
 		count += 1
 	# Calculate sampling period
 	curr_time = time.time()
-	T = curr_time - prev_time
-
+	#T = curr_time - prev_time
+	T = 0.02
 
 	# Kalman filter for vertical acceleration estimation
 
 	# Prediction update with data from previous iteration and sensorss
-	z = predict_state(gyro, z_prev, T) # State prediction
-	#z /= la.norm(z)
+	z = predict_state(gyro_prev, z_prev, T) # State prediction
+	z /= la.norm(z)
 	P = predict_error_covariance(gyro_prev, z_prev, T, P, sigma_gyro)
 	# Measurement update
-	K = update_kalman_gain(P, H, ca, a, sigma_accel)
-	measurement = accel - ca*a
+	K = update_kalman_gain(P, H, ca, a_sensor, sigma_accel)
+	measurement = accel - ca*a_sensor
 	z = update_state_with_measurement(z, K, measurement, H)
 	z /= la.norm(z)
 	P = update_error_covariance(P, H, K)
+
 	# compute the acceleration from the estimated value of z
-	a = accel - g*z
+	a_sensor = accel - g*z
 	# Acceleration in earth reference frame
-	a_earth = a.dot(z)
+	a_earth = np.vdot(a_sensor, z)
 
 	# Complementary filter for altitude and vertical velocity estimation
 
