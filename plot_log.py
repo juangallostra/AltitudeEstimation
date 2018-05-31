c5c05c79e6a07951838988141c49596257a94641
Juan Gallostra <jgallostra@naralabs.com>
2018-05-31 10:58:28 +0200

    Change plot colors

--- plot_log.py
+++ plot_log.py
@@ -17,8 +17,8 @@ ax1.set_title("acceleration, velocity and height estimation")
 ax1.set_xlabel('Sample')
 ax1.set_ylabel('Value')
 
-ax1.plot(height_from_baro, c='y', label='Height from baro')
-ax1.plot(accel, c='b', label='Acceleration')
+ax1.plot(height_from_baro, c='#99ffe6', label='Height from baro')
+ax1.plot(accel, c='#ffa366', label='Acceleration')
 ax1.plot(vel, c='r', label='Velocity')
 ax1.plot(height, c='g', label='Height')
 
