6fe55b7913569f3e9153a694fdbe1325034ca40f
juangallostra <juangallostra@gmail.com>
2018-06-12 16:58:33 +0200

    Start working on an example

---
+++ examples/SimpleEstimation/SimpleEstimation.ino
@@ -0,0 +1,15 @@
+/*
+   AltitudeEstimation.ino : Arduino sketch to perform altitude estimation using
+   the provided library
+ */
+
+#include <Arduino.h>
+#include "altitude.hpp"
+
+void setup(void)
+{
+}
+
+void loop(void)
+{
+}
