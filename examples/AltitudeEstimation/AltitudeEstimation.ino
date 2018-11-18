3af8e95f2510de6d98566fa0c267004173d58549
Juan Gallostra <juangallostra@gmail.com>
2018-11-18 16:36:09 +0100

    Algorithm working with new components

--- examples/AltitudeEstimation/AltitudeEstimation.ino
+++ examples/AltitudeEstimation/AltitudeEstimation.ino
@@ -4,135 +4,78 @@
  */
 
 #include <Arduino.h>
-#include <ArduinoTransfer.h>
 #include <Wire.h>
-// Assuming the IMU is an MPU9250, the baro a MS5637 and the rangefinder a VL53L1X
-#include <MPU9250.h>
-#include <MS5637.h>
+// IMU
+#include <LSM6DSM.h>
+// Barometer
+#include <LPS22HB.h>
 #include <VL53L1X.h>
 // For quaternion computation
-#include <MadgwickAHRS.hpp>
+#include <quaternion.hpp>
 
 #include "altitude.h"
 
-// helper variables and functions for obtaining baro data
-static const uint8_t HISTORY_SIZE = 48;
+uint8_t LED_PIN = 38;
 
-static float   groundAltitude = 0;
-static float   groundPressure = 0;
-static float   pressureSum = 0;
-static float   history[HISTORY_SIZE];
-static uint8_t historyIdx = 0;
-static int     endCalibration = 120;
+// Global constants for 6 DoF quaternion filter
+const float GYRO_MEAS_ERROR = M_PI * (40.0f / 180.0f);
+const float GYRO_MEAS_DRIFT = M_PI * (0.0f  / 180.0f);
+const float BETA = sqrtf(3.0f / 4.0f) * GYRO_MEAS_ERROR; 
+const float ZETA = sqrt(3.0f / 4.0f) * GYRO_MEAS_DRIFT;
 
-static MS5637 barometer = MS5637();
+// Quaternion support: even though IMU has a magnetometer, we keep it simple for now by 
+// using a 6DOF fiter (accel, gyro)
+quat::MadgwickQuaternionFilter6DOF _quaternionFilter = quat::MadgwickQuaternionFilter6DOF(BETA, ZETA);
 
-// Pressure in millibars to altitude in meters. We assume
-// millibars are the units of the pressure readings from the sensor
-static float millibarsToMeters(float mbar)
+void getQuaternion(float quat[4], float accel[3], float gyro[3], float deltat)
 {
-    // see: https://www.weather.gov/media/epz/wxcalc/pressureAltitude.pdf
-    return (1.0f - powf(mbar / 1013.25f, 0.190295f)) * 44330.0f;
+    // Run the quaternion on the IMU values acquired in imuRead()                   
+    _quaternionFilter.update(accel[0], accel[1], accel[2], gyro[0], gyro[1], gyro[2], deltat); 
+
+    // Copy the quaternion back out
+    quat[0] = _quaternionFilter.q1;
+    quat[1] = _quaternionFilter.q2;
+    quat[2] = _quaternionFilter.q3;
+    quat[3] = _quaternionFilter.q4;
 }
 
-// Calibrate the baro by setting the average measured pressure as the
-// ground pressure and the corresponding altitude as the ground altitude.
-static void calibrate(float pressure)
+// --- IMU related variables and functions ---
+// LSM6DSM settings
+static const LSM6DSM::Ascale_t Ascale = LSM6DSM::AFS_2G;
+static const LSM6DSM::Gscale_t Gscale = LSM6DSM::GFS_2000DPS;
+static const LSM6DSM::Rate_t   AODR   = LSM6DSM::ODR_1660Hz;
+static const LSM6DSM::Rate_t   GODR   = LSM6DSM::ODR_1660Hz;
+
+// Biases
+float ACCEL_BIAS[3] = {0.0, 0.0, 0.0};
+float GYRO_BIAS[3]  = {0.0, 0.0, 0.0};
+
+LSM6DSM lsm6dsm = LSM6DSM(Ascale, Gscale, AODR, GODR, ACCEL_BIAS, GYRO_BIAS);
+
+static void getGyrometerAndAccelerometer(float gyro[3], float accel[3], float _q[4], float deltat)
 {
-    // Update pressure history
-    history[historyIdx] = pressure;
-    pressureSum += pressure;
-    // cycle the index throught the history array
-    uint8_t nextIndex = (historyIdx + 1) % HISTORY_SIZE;
-    // Remove next reading from sum so that pressureSum is kept in sync
-    pressureSum -= history[nextIndex];
-    historyIdx = nextIndex;
-    // groundPressure will stabilize at 8 times the average measured
-    // pressure (when groundPressure/8 equals pressureSum/(HISTORY_SIZE-1))
-    // This acts as a low pass filter and helps to reduce noise
-    groundPressure -= groundPressure / 8;
-    groundPressure += pressureSum / (HISTORY_SIZE - 1);
-    groundAltitude = millibarsToMeters(groundPressure/8);
-}
+    float _ax, _ay, _az, _gx, _gy, _gz;
+    
+    if (lsm6dsm.checkNewData()) {
+        lsm6dsm.readData(_ax, _ay, _az, _gx, _gy, _gz);
 
-static float getAltitude(float pressure)
-{
-    return  (millibarsToMeters(pressure) - groundAltitude);
-}
+        // Negate to support board orientation
+        _ax = -_ax;
+        _gy = -_gy;
+        _gz = -_gz;
 
-// helper variables and functions for obtaining IMU data
-// Sensor full-scale settings
-static const Ascale_t ASCALE = AFS_8G;
-static const Gscale_t GSCALE = GFS_2000DPS;
-static const Mscale_t MSCALE = MFS_16BITS;
-static const Mmode_t MMODE = M_100Hz;
-// SAMPLE_RATE_DIVISOR: (1 + SAMPLE_RATE_DIVISOR) is a simple divisor of the fundamental 1000 kHz rate of the gyro and accel, so
-// SAMPLE_RATE_DIVISOR = 0 means 1 kHz sample rate for both accel and gyro, 4 means 200 Hz, etc.
-static const uint8_t SAMPLE_RATE_DIVISOR = 0;
-// MPU9250 add-on board has interrupt on Butterfly pin 8
-static const uint8_t INTERRUPT_PIN = 8;
-// Create byte-transfer objects for Arduino I^2C
-static ArduinoI2C mpu = ArduinoI2C(MPU9250::MPU9250_ADDRESS);
-static ArduinoI2C mag = ArduinoI2C(MPU9250::AK8963_ADDRESS);
+        // Copy gyro values back out in rad/sec
+        gyro[0] = _gx * M_PI / 180.0f;
+        gyro[1] = _gy * M_PI / 180.0f;
+        gyro[2] = _gz * M_PI / 180.0f;
+        // and acceleration values
+        accel[0] = _ax;
+        accel[1] = _ay;
+        accel[2] = _az;
 
-// Use the MPU9250 in pass-through mode
-static MPU9250Passthru imu = MPU9250Passthru(&mpu, &mag);;
-// Store imu data
-static int16_t imuData[7] = {0,0,0,0,0,0,0};
-// For scaling to normal units (accelerometer G's, gyrometer rad/sec, magnetometer mGauss)
-static float aRes;
-static float gRes;
-static float mRes;
-// We compute these at startup
-static float gyroBias[3]  = {0,0,0};
-static float accelBias[3] = {0,0,0};
-// flag for when new data is received
-static bool gotNewData = false;
-
-static void interruptHandler()
-{
-    gotNewData = true;
-}
-
-// Raw analog-to-digital values converted to radians per second
-static float adc2rad(int16_t adc, float bias)
-{
-    return (adc * gRes - bias) * M_PI / 180;
-}
-
-static void getGyrometerAndAccelerometer(float gyro[3], float accel[3], float _q[4])
-{
-    if (gotNewData) {
-
-        gotNewData = false;
-
-        if (imu.checkNewAccelGyroData()) {
-
-            imu.readMPU9250Data(imuData);
-
-            // Convert the accleration value into g's
-            float ax = imuData[0]*aRes - accelBias[0];  // get actual g value, this depends on scale being set
-            float ay = imuData[1]*aRes - accelBias[1];
-            float az = imuData[2]*aRes - accelBias[2];
-
-            // Convert the gyro value into degrees per second
-            float gx = adc2rad(imuData[4], gyroBias[0]);
-            float gy = adc2rad(imuData[5], gyroBias[1]);
-            float gz = adc2rad(imuData[6], gyroBias[2]);
-
-            // Copy gyro values back out
-            gyro[0] = gx;
-            gyro[1] = gy;
-            gyro[2] = gz;
-            // and acceleration values
-            accel[0] = ax;
-            accel[1] = ay;
-            accel[2] = az;
-
-            // Update quaternion estimation
-            MadgwickAHRSupdateIMU(_q, gx, -gy, -gz, -ax, ay, az);
-        } // if (imu.checkNewAccelGyroData())
-    } // if gotNewData
+    } 
+    // Update quaternion estimation
+    getQuaternion(_q, accel, gyro, deltat);
 }
 
 static VL53L1X distanceSensor;
@@ -148,13 +91,14 @@ static void computeEulerAngles(float q[4], float euler[3])
 
 // Altitude estimator
 static AltitudeEstimator altitude = AltitudeEstimator(0.0005, // sigma Accel
-        0.0005, // sigma Gyro
-        0.001,   // sigma Range
-        0.5, // ca
-        0.1);// accelThreshold
+        0.0005,   // sigma Gyro
+        0.0001,    // sigma Range
+        0.5,      // ca
+        0.1);     // accelThreshold
 
 // We will control when the data is sent through serial
 float printTime = millis();
+float currentTime = millis();
 
 void setup(void)
 {
@@ -163,57 +107,32 @@ void setup(void)
     Wire.setClock(400000); // I2C frequency at 400 kHz
     delay(1000);
 
-    // Reset the MPU9250
-    imu.resetMPU9250();
-
-    // get sensor resolutions, only need to do this once
-    aRes = imu.getAres(ASCALE);
-    gRes = imu.getGres(GSCALE);
-    mRes = imu.getMres(MSCALE);
-
-    // Calibrate gyro and accelerometers, load biases in bias registers
-    imu.calibrateMPU9250(gyroBias, accelBias);
-
-    // Initialize the MPU9250
-    imu.initMPU9250(ASCALE, GSCALE, SAMPLE_RATE_DIVISOR);
-    // Set all pressure history entries to 0
-    for (uint8_t k = 0; k < HISTORY_SIZE; ++k) {
-        history[k] = 0;
-    }
-    // begin Barometer
-    barometer.begin();
-
-    // calibrate barometer
-    for (uint8_t k=0; k <= endCalibration; k++) {
-        float readPressure;
-        barometer.getPressure(& readPressure);
-        calibrate(readPressure);
-    }
     // Begin serial comms
     Serial.begin(115200);
-    // Set up the interrupt pin, it's set as active high, push-pull
-    pinMode(INTERRUPT_PIN, INPUT);
-    attachInterrupt(INTERRUPT_PIN, interruptHandler, RISING);
-
+    // initialize sensors
+    lsm6dsm.begin();
+    lsm6dsm.calibrate(GYRO_BIAS, ACCEL_BIAS);
     if (distanceSensor.begin() == false) {
         while (true) {
             Serial.println("Sensor offline!");
             delay(200);
         }
     }
-
 }
 
 void loop(void)
 {
+  // print data every 20 millis
+  // (Lidar update frequency is 50Hz)
+  currentTime = millis();
+  float deltat = currentTime - printTime;
+  if (deltat > 20)
+  {
     // get all necessary data
-    //float pressure;
-    //barometer.getPressure(& pressure);
-    //float baroHeight = getAltitude(pressure);
     float accelData[3];
     float gyroData[3];
     float quaternion[4];
-    getGyrometerAndAccelerometer(gyroData, accelData, quaternion);
+    getGyrometerAndAccelerometer(gyroData, accelData, quaternion, deltat / 1000.0f);
     float rangeHeight = (float)distanceSensor.getDistance() / 1000.0f;
     uint32_t timestamp = micros();
     // Compensate for effect of pitch, roll on rangefinder reading
@@ -221,17 +140,13 @@ void loop(void)
     computeEulerAngles(quaternion, euler);
     rangeHeight =  rangeHeight * cos(euler[0]) * cos(euler[1]);
     altitude.estimate(accelData, gyroData, rangeHeight, timestamp);
-    float currentTime = millis();
-    // print data every 5 millis
-    if (currentTime-printTime > 5)
-    {
-        Serial.print(rangeHeight);
-        Serial.print(",");
-        Serial.print(altitude.getAltitude());
-        Serial.print(",");
-        Serial.print(altitude.getVerticalVelocity());
-        Serial.print(",");
-        Serial.println(altitude.getVerticalAcceleration());
-        printTime = currentTime;
-    }
+    Serial.print(rangeHeight);
+    Serial.print(",");
+    Serial.print(altitude.getAltitude());
+    Serial.print(",");
+    Serial.print(altitude.getVerticalVelocity());
+    Serial.print(",");
+    Serial.println(altitude.getVerticalAcceleration());
+    printTime = currentTime;
+  }
 }
